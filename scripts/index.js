2fc0bac41683abde4c1a49cfadd1f7f5bd813b7c
Parth Agarwal <agarwal.parth.101@protonmail.com>
2020-08-06 23:37:10 +0530

    final version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 let progress = document.getElementById("progressbar");
+let passiveList = document.getElementsByClassName("passive")
+
 let totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function () {
+
+
+function scrolly() {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
+
+    if (passiveList.length > 0) {
+        if (window.pageYOffset + (window.innerHeight / 2) > passiveList[0].offsetTop) {
+            if (passiveList[0].id == "toolbox" || passiveList[0].id == "box") {
+                passiveList[0].classList.replace("passive", "active-left")
+            }
+            else {
+                passiveList[0].classList.replace("passive", "active-top")
+            }
+        }
+        if (progressHeight >= 90) {
+            for (let i = 0; i < passiveList.length; i++) {
+                passiveList[i].classList.replace("passive", "active-left")
+            }
+        }
+    }
 }
+
+
+addEventListener("scroll", scrolly)
+
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
