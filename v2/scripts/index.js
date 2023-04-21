e80e8c73723bae26827dd6769447d558baeb50a2
Parth Agarwal <agarwal.parth.101@protonmail.com>
2023-04-21 11:10:19 +0530

    update v0 v1 v2

---
+++ v2/scripts/index.js
@@ -0,0 +1,39 @@
+let progress = document.getElementById("progressbar");
+let passiveList = document.getElementsByClassName("passive")
+let bg = document.getElementById("bg");
+
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+
+function scrolly() {
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+
+    if (passiveList.length > 0) {
+        if (window.pageYOffset + (2 * window.innerHeight / 3) > passiveList[0].offsetTop) {
+            if (passiveList[0].id == "toolbox" || passiveList[0].id == "box") {
+                passiveList[0].classList.replace("passive", "active-left")
+            }
+            else {
+                passiveList[0].classList.replace("passive", "active-top")
+            }
+        }
+        if (progressHeight >= 90) {
+            for (let i = 0; i < passiveList.length; i++) {
+                passiveList[i].classList.replace("passive", "active-left")
+            }
+        }
+    }
+}
+
+addEventListener("scroll", scrolly)
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("ip_add").innerHTML = this.responseText;
+    }
+};
+xhttp.open("GET", "https://api.ipify.org/", true);
+xhttp.send();
